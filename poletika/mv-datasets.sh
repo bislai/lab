249256ae91d3c26140a77095d2f8947e0c5dd299
Jorge Aznar <jorgeatgu@gmail.com>
2019-03-19 22:33:57 +0100

    create script to move datasets

--- poletika/mv-datasets.sh
+++ poletika/mv-datasets.sh
@@ -14,11 +14,11 @@ partidosre=('unidas-podemos' 'partido-socialista' 'ciudadanos' 'partido-popular'
 # La ruta donde tenemos los archivos
 pathPartidos="$(pwd)/partidos/"
 
-# La urta donde los vamos a mover
+# La ruta donde los vamos a mover los datasets que hemos creado
 github="$HOME/github/pqnvl/csv/"
 
 for ((i = 0; i < ${#partidosre[@]}; i++)); do
 
-    mv "$pathPartidos""${partidosre[$i]}"/"${partidosre[$i]}"-total-propuestas.csv "$github""${partidosre[$i]}"/"${partidosre[$i]}"-total-propuestas.csv
+    mv "$pathPartidos""${partidosre[$i]}"/"${partidosre[$i]}".json "$github""${partidosre[$i]}"/"${partidosre[$i]}".json
 
 done
